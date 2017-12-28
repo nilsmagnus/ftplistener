1e482517d3d26b17e37b4ba9a1a9c7d4accce5cb
Nils Larsgård <nilsmagnus@gmail.com>
2017-12-28 14:01:36 +0100

    Initial commit. Working application.

---
+++ README.md
@@ -0,0 +1,41 @@
+get latest grib2 files from nooa via anonymous ftp (ftp.ncep.noaa.gov)
+
+# prereqs
+
+* go 1.8+ installed
+* dep installed
+
+# build & run
+
+
+    dep ensure
+    go build
+    ./ftplistener
+    
+
+* will get dependencies(ftp-client), build the application and start downloading all current gribfiles from nooa. 
+* checks for duplicates before downloading
+
+# usage
+    Usage of ./ftplistener:
+      -baseDir string
+        	Base dir (default "/pub/data/nccf/com/gfs/prod")
+      -destination string
+        	destination for downloaded files (default "gribfiles")
+      -host string
+        	Ftp host to connect to (default "ftp.ncep.noaa.gov")
+      -password string
+        	ftp password (default "anything")
+      -port string
+        	Ftp port to connect to (default "21")
+      -user string
+        	ftp user (default "anonymous")
+
+
+# gotchas
+
+* downloads only 1p00 files, change the source if you need something else
+* will not verify size/checksum of files
+* all files for 1 day is about 4GB in size
+
+    
---
+++ main.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+	"github.com/jlaffaye/ftp"
+	"flag"
+	"fmt"
+	"regexp"
+	"strings"
+	"os"
+	"time"
+	"io"
+)
+
+func main() {
+
+	hostName := flag.String("host", "ftp.ncep.noaa.gov", "Ftp host to connect to")
+	port := flag.String("port", "21", "Ftp port to connect to")
+	baseDir := flag.String("baseDir", "/pub/data/nccf/com/gfs/prod", "Base dir")
+	user := flag.String("user", "anonymous", "ftp user")
+	password := flag.String("password", "anything", "ftp password")
+	saveFolder := flag.String("destination", "gribfiles", "destination for downloaded files")
+
+	flag.Parse()
+
+	host := fmt.Sprintf("%s:%s", *hostName, *port)
+
+	conn, connectErr := connect(host, *user, *password)
+
+	if connectErr != nil {
+		panic(connectErr)
+	}
+
+	list, listErr := conn.List(*baseDir)
+
+	if listErr != nil {
+		panic(listErr)
+	}
+
+	gfsFolderName, _ := regexp.Compile("gfs.([0-9]{8})")
+
+	for _, entry := range list {
+		if folderIsRelevant(entry, gfsFolderName) {
+			fmt.Printf("->\thit folder %s\n", entry.Name)
+			if files, err := listFiles(conn, *baseDir, entry.Name); err == nil {
+				downloadAll(conn, files, *saveFolder)
+			}
+		}
+	}
+
+}
+func downloadAll(conn *ftp.ServerConn, entries []*ftp.Entry, destinationFolder string) {
+	for _, fileEntry := range entries {
+		if _, err := os.Stat(filePath(destinationFolder, fileEntry)); os.IsNotExist(err) {
+			downloadSingle(conn, fileEntry, destinationFolder)
+		}
+	}
+}
+
+func downloadSingle(conn *ftp.ServerConn, entry *ftp.Entry, destinationFolder string) error {
+	os.MkdirAll(fileFolder(destinationFolder, entry.Time), 0777)
+
+	response, err := conn.Retr(entry.Name)
+	if err != nil {
+		return err
+	}
+	defer response.Close()
+
+	fileName := filePath(destinationFolder, entry)
+
+	file, ferr := os.Create(fileName)
+	if ferr != nil {
+		return ferr
+	}
+
+	defer file.Close()
+	_, writeErr := io.Copy(file, response) // todo inspect content if you want to here
+
+	if writeErr != nil {
+		return writeErr
+	}
+	return nil
+}
+
+func fileFolder(folderName string, entryTime time.Time) (string) {
+	return fmt.Sprintf("%s/%d%2d%2d%2d/", folderName, entryTime.Year(), entryTime.Month(), entryTime.Day(), entryTime.Hour())
+}
+func filePath(folderName string, entry *ftp.Entry) string {
+
+	return fmt.Sprintf("%s%s", fileFolder(folderName, entry.Time), entry.Name)
+}
+func listFiles(conn *ftp.ServerConn, baseDir string, subDir string) ([]*ftp.Entry, error) {
+
+	dirErr := conn.ChangeDir(baseDir + "/" + subDir)
+	if dirErr != nil {
+		return nil, dirErr
+	}
+
+	list, err := conn.List("")
+
+	if err != nil {
+		return nil, err
+	}
+
+	gfsFileName, _ := regexp.Compile("gfs.t([0-9]{2})z.pgrb2.1p00.f([0-9]{3})") // TODO parameterize this pattern?
+
+	relevantList := make([]*ftp.Entry, 0)
+	for _, e := range list {
+		if e.Type == ftp.EntryTypeFile && gfsFileName.MatchString(e.Name) && !strings.Contains(e.Name, "idx") {
+			relevantList = append(relevantList, e)
+		}
+	}
+
+	return relevantList, nil
+
+}
+
+func folderIsRelevant(l *ftp.Entry, gfsFolderName *regexp.Regexp) bool {
+	return l.Type == ftp.EntryTypeFolder && gfsFolderName.MatchString(l.Name)
+}
+func connect(host, user, password string) (*ftp.ServerConn, error) {
+	conn, err := ftp.Dial(host)
+	if err != nil {
+		return nil, err
+	}
+	loginErr := conn.Login(user, password)
+	if loginErr != nil {
+		return nil, loginErr
+	}
+	return conn, nil
+}
